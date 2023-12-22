9259b8a5c2c79ad26edba3afb2fb377c1f4d7380
felixjff <felixfariasfueyo@hotmail.com>
2023-12-22 10:25:32 +0100

    adjustments

--- my_integrations/origin_protocol/oswap_v1/oswap_v1_model.py
+++ my_integrations/origin_protocol/oswap_v1/oswap_v1_model.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from my_integrations.origin_protocol.oswap_v1.oswap_v1_helper import OSwapV1Helper
 
 
@@ -14,7 +16,10 @@ class OSwapV1Model:
         self.balance0 = 0
         self.balance1 = 0
 
-    def to_dict(self, deep=False):
+    def to_dict(
+        self, 
+        deep=False
+    ) -> dict:
         data = {
             "pool_address": self.pool_address,
             "tokens": self.tokens,
@@ -28,10 +33,15 @@ class OSwapV1Model:
             data["balance1"] = self.balance1
         return data
 
-    def has_complete_data(self):
+    def has_complete_data(self) -> bool:
         return self.traderate0 > 0 and self.traderate1 > 0
 
-    def get_amount_out(self, sell_token, sell_amount):
+    def get_amount_out(
+        self, 
+        sell_token: str, 
+        sell_amount: int
+    ) -> Tuple[int, int]:
+        
         inToken = sell_token
         amountIn = sell_amount
         price = 0
@@ -40,15 +50,20 @@ class OSwapV1Model:
         elif inToken == self.tokens[1]:
             price = self.traderate1
         else:
-            return [0, 0]
+            return 0, 0
         amountOut = int(amountIn) * int(price) // 10**36
         if inToken == self.tokens[0] and amountOut > self.balance1:
-            return [0, 0]
+            return 0, 0
         elif inToken == self.tokens[1] and amountOut > self.balance0:
-            return [0, 0]
-        return [0, amountOut]
+            return 0, 0
+        return 0, amountOut
 
-    def get_amount_in(self, buy_token, buy_amount):
+    def get_amount_in(
+        self, 
+        buy_token:str, 
+        buy_amount:int
+    ) -> Tuple[int,int]:
+        
         outToken = buy_token
         amountOut = buy_amount
         price = 0
@@ -57,20 +72,32 @@ class OSwapV1Model:
         elif outToken == self.tokens[0]:
             price = self.traderate1
         else:
-            return [0, 0]
+            return 0, 0
         amountIn = int(amountOut) * 10**36 // int(price) + 1
         if outToken == self.tokens[0] and amountOut > self.balance0:
-            return [0, 0]
+            return 0, 0
         elif outToken == self.tokens[1] and amountOut > self.balance1:
-            return [0, 0]
-        return [0, amountIn]
+            return 0, 0
+        return 0, amountIn
 
-    def get_state_calls(self):
+    def get_state_calls(self) -> list:
         return [OSwapV1Helper.get_state_call()]
 
-    def process_rpc_data(self, data):
-        results = OSwapV1Helper.get_state_call(data)
-        self.traderate0 = results.traderate0
-        self.traderate1 = results.traderate1
-        self.balance0 = results.balance0
-        self.balance1 = results.balance1
+    def process_rpc_data(
+        self, 
+        data: dict
+    ):
+        '''
+            data -> {'result':str, 'attribute': str}
+        '''
+        if data['attribute'] == 'states' and data['result']:
+
+            results = OSwapV1Helper.get_state_call(data['result'])
+            self.traderate0 = results.traderate0
+            self.traderate1 = results.traderate1
+            self.balance0 = results.balance0
+            self.balance1 = results.balance1
+
+        else:
+
+            raise Exception("### ERROR: Uknown rpc data attribute for OSwapV1")
